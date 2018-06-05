e5c21e26ee140aae3dfb4c5cb9b18e2a769e54c8
Dragunov314 <a22451648@gmail.com>
2018-06-05 16:49:49 +0800

    Upload program

---
+++ CRC16CCITT/CRC16CCITT/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace CRC16CCITT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.Label();
+            this.btnCalulate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 44);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(246, 292);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "13 00 1B 00 00 0A 01 00 02 00 00 0A 01 00 01 FF 00 02 02 01 FF FF 00 00 01";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Input HEX";
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Location = new System.Drawing.Point(13, 341);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(135, 19);
+            this.result.TabIndex = 2;
+            this.result.Text = "CRC16-CCITT = ";
+            // 
+            // btnCalulate
+            // 
+            this.btnCalulate.Location = new System.Drawing.Point(292, 310);
+            this.btnCalulate.Name = "btnCalulate";
+            this.btnCalulate.Size = new System.Drawing.Size(104, 26);
+            this.btnCalulate.TabIndex = 0;
+            this.btnCalulate.Text = "Calculate";
+            this.btnCalulate.UseVisualStyleBackColor = true;
+            this.btnCalulate.Click += new System.EventHandler(this.btnCalulate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(292, 278);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(104, 26);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 369);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalulate);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "CRC16CCITT Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.Button btnCalulate;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ CRC16CCITT/CRC16CCITT/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRC16CCITT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalulate_Click(object sender, EventArgs e)
+        {
+            string[] hexValues = textBox1.Text.Split(' ');
+            byte[] hexValuesByte = new byte[hexValues.Length];            
+
+            for(int i=0;i<hexValues.Length;i++)
+            {                
+                hexValuesByte[i] = Convert.ToByte(Convert.ToInt32(hexValues[i], 16));
+            }
+            //byte[] x = hexValuesByte;
+            
+            result.Text = "CRC16-CCITT = 0x" + Compute_CRC16_Simple(hexValuesByte).ToString("X4");
+
+            //byte[] datalength = new byte[] { 19, 0, 0 };
+            //int a = Convert.ToInt32(datalength[0]);
+            //int tmt = a + 1;
+
+        }
+        public ushort Compute_CRC16_Simple(byte[] bytes)
+        {
+            /*
+            Reference : http://www.sunshine2k.de/articles/coding/crc/understanding_crc.html
+            Algorithm : CRC-16-CCIT-FALSE
+            Polynomial : 0x1021
+            Initial Value : 0xFFFF
+            Final XOR Value : 0x0 (Actually 0 has no impact)            
+            */
+            const ushort generator = 0x1021;    /* divisor is 16bit */
+            ushort crc = 0xFFFF; /* CRC value is 16bit (Initial Value)*/
+
+            foreach (byte b in bytes)
+            {
+                crc ^= (ushort)(b << 8); /* move byte into MSB of 16bit CRC */
+
+                for (int i = 0; i < 8; i++)
+                {
+                    if ((crc & 0x8000) != 0) /* test for MSB = bit 15 */
+                    {
+                        crc = (ushort)((crc << 1) ^ generator);
+                    }
+                    else
+                    {
+                        crc <<= 1;
+                    }
+                }
+            }
+
+            return crc;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+    }
+}
